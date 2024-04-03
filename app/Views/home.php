0bfa6402e4edf4f48a5eb2b9bfe15e02deb93d69
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-03 13:42:30 -0300

    Correções da página inicial

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home</h1>
-</body>
-</html>
+<?= $this->extend('principal') ?>
+
+
+<?= $this->section('conteudo') ?>
+   <h1>Home</h1>
+<?= $this->endSection() ?>
---
+++ app/Views/principal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FILMES</title>
+    <link rel="stylesheet" href="<?= base_url('public/assets/css/bootstrap.min.css') ?>" type="text/css">
+   
+</head>
+<body>
+    <h1>FILMES</h1>
+
+    <div class="container ht-5">
+        <h3 class="text-center bg-black p-3 mb-5">Filmes</h3>
+        <div class="row">
+            <div class="col">
+                <?= $this->renderSection('conteudo') ?>
+            </div>
+
+    <p class="text-center mt-5">CUIDADOR DE IDOSOS &copy <?= date('Y') ?></p>
+
+
+        </div>
+</body>
+</html>
